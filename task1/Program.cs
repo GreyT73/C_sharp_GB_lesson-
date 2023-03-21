b28b09a34c7b2569465bf1c8cbf3cf54115a0738
Sergey <greyt73@yandex.ru>
2023-03-21 22:47:55 +0300

    Add task

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 3;
+int maximum = a;
+if (b > maximum) maximum = b;
+if (c > maximum) maximum = c;
+if (d > maximum) maximum = d;
+if (e > maximum) maximum = e;
+Console.Write("Maximum is:");
+Console.WriteLine(maximum);
