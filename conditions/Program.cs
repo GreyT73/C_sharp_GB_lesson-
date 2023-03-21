99b6f173441b31b97580ea642ee12ac66e47ddf2
Sergey <greyt73@yandex.ru>
2023-03-21 22:32:36 +0300

    Add conditions

---
+++ conditions/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter user name: ");
+string user = Console.ReadLine();
+if(user.ToLower() == "sergey")
+{
+    Console.WriteLine("OMG! This is Sergey <3");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(user);
+}
