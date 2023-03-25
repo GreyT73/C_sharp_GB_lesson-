3ef7ea1257b10f439365dd04f1f54920864a8caa
Sergey <greyt73@yandex.ru>
2023-03-25 21:43:01 +0300

    Add task with arguments

---
+++ maximumOfNine/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 1209;
+int a2 = 32;
+int a3 = 31;
+int a4 = 21;
+int a5 = 43;
+int a6 = 42;
+int a7 = 33;
+int a8 = 28;
+int a9 = 301;
+
+int max1 = Max(a1, a2, a3);
+int max2 = Max(a4, a5, a6);
+int max3 = Max(a7, a8, a9);
+int max = Max(max1, max2, max3);
+
+
+Console.WriteLine(max);
